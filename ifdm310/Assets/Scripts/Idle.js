c8431846063a5c5dde85a3e632f2c0140fe91cde
Ally <star.goodman@gmail.com>
2017-01-24 17:21:11 -0700

    added robot and background

---
+++ ifdm310/Assets/Scripts/Idle.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+var anim: Animator;
+var facingRight = true;
+public var speed:float;
+
+function Start () {
+	
+}
+
+function Update () {
+	var moveH = Input.GetAxis("Horizontal")*Time.deltaTime * speed;
+	var moveV = Input.GetAxis("Vertical")*Time.deltaTime * speed;
+	transform.Translate(moveH,moveV,0);
+
+	//Facing right way
+     if(moveH < 0 && facingRight)
+         Flip();
+     else if(moveH > 0 && !facingRight)
+         Flip();
+}
+
+function Flip()
+ {
+     facingRight = !facingRight;
+     var theScale = transform.localScale;
+     theScale.x *= -1;
+     transform.localScale = theScale;
+ }
