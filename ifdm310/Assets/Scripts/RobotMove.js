55553ff58ad9150ee87eae0c1df7091f2617eebb
Ally <star.goodman@gmail.com>
2017-01-30 15:43:26 -0700

    added comments on robotMove

--- ifdm310/Assets/Scripts/RobotMove.js
+++ ifdm310/Assets/Scripts/RobotMove.js
@@ -5,12 +5,12 @@ var facingRight = false;
 public var speed:float;
 
 var touchList: Transform[];
-var touched = 1;
+private var touched = 1;
 
 var currentTouch: Transform;
 var targetTouch: Transform;
 
-var walkToLeft = true;
+private var walkToLeft = true;
 private var walking = true;
 
 
@@ -47,24 +47,29 @@ function move()
 {
 	var step = speed * Time.deltaTime;
 
+	//Make robot move towards target
 	if(walking) transform.position = Vector3.MoveTowards(transform.position, targetTouch.position, step);
 
-
+	//What happens when robot reaches target
 	 if(transform.position == targetTouch.position)
     {    
-
+   		//If it gets to the pause target, robot pauses to scan room
     	if(targetTouch == touchList[1]){
-    		print("touched halfway");
+    		print("scanning room");
     	 	scanRoom();
     	 }
-    		
+
+    	//Keeps track of which way the robot should be touching targets
+    	//Goes 1, 2, 3, 2, 1, 2, 3, etc.
     	if(walkToLeft){
     		touched++;
     	}
     	else touched--;
 
+    	//Checks for end targets and makes robot walk other direction
     	if(touched == touchList.length-1 || touched == 0) walkToLeft = !walkToLeft;
 
+    	//Switches out targets once touched
         currentTouch = targetTouch;
         targetTouch = touchList[touched];
 
@@ -75,10 +80,13 @@ function scanRoom(){
 	walking = false;
 	anim.SetFloat("Speed", 0.0);
 
+	/* Once we have the light beam eye-scanner animation, it will go here */
+
 	yield WaitForSeconds (8);
 	walking = true;
 }
 
+//Flips sprite across y-axis
 function Flip()
  {
      facingRight = !facingRight;
