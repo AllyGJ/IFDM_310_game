fb5241a1371026c257e86c208c79adf1477bce66
Ally <star.goodman@gmail.com>
2017-01-31 23:46:08 -0700

    added cutscene and movie player

---
+++ ifdm310/Assets/Video.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+@script RequireComponent(AudioSource)
+
+
+public var movie: MovieTexture;
+
+function Start () {
+	GetComponent.<Renderer>().material.mainTexture = movie as MovieTexture;
+	movie.Play();
+}
+
+
