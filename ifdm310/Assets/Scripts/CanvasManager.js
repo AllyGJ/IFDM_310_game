ba5190908a8251a581de004b21315a95591e6685
Ally <star.goodman@gmail.com>
2017-04-01 15:59:20 -0600

    popup window on robot collision

--- ifdm310/Assets/Scripts/CanvasManager.js
+++ ifdm310/Assets/Scripts/CanvasManager.js
@@ -2,6 +2,8 @@
 
 public static var instance = null;
 
+public var deadWindow: GameObject;
+
 function Awake(){
 
 	if(instance == null) instance = this;
@@ -13,9 +15,24 @@ function Awake(){
 }
 
 function Start () {
-	
+	deadWindow.SetActive(false);
 }
 
 function Update () {
 	
+}
+
+
+public function showDeathWindow(val : boolean)
+{
+	deadWindow.SetActive(val);
+}
+
+public function startOver(){
+
+}
+
+public function exit()
+{
+
 }
--- ifdm310/Assets/Scripts/girl/PRCollision.js
+++ ifdm310/Assets/Scripts/girl/PRCollision.js
@@ -1,12 +1,18 @@
 ﻿#pragma strict
 
-//public var enemyList: Collider2D[];
 
+private var deathWindow: CanvasManager;
+
+function Start()
+{
+	deathWindow = GameObject.Find("canvas").GetComponent(CanvasManager);
+}
 
 function OnTriggerEnter2D(Col: Collider2D)
 {
 	if(Col.CompareTag("Robot") || Col.CompareTag("lazerBeam"))
 	{
 		print("DEAD");
+		deathWindow.showDeathWindow(true);
 	}
 }
